30dcc41ea05865a8575c03825e93d1c5a203cd4b
vilder <vilder.van@gmail.com>
2026-07-09 19:14:54 +0200

    tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
-    tailwind()
   ],
   output: 'static', // Build as static site (SSG)
   vite: {
+    plugins: [tailwindcss()],
     envPrefix: ['PUBLIC_', 'VITE_'],
   }
 });
